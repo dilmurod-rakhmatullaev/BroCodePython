893677c59563b470d6a655783862065cc4d63869
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-04-11 21:34:03 +0300

    Add basic input validation using while loops

---
+++ lesson_03/while_loops.py
@@ -0,0 +1,32 @@
+name = input("Enter your name: ")
+
+while name == "":
+    print("You did not enter your name, please try again")
+    name = input("Enter your name: ")
+else:
+    print(f"Hello {name}")
+
+age = int(input("Enter your age: "))
+while age < 0:
+    print("Age can not be negative")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old")
+
+
+food = input("Enter a food you like (q to quit): ")
+
+while food != "q":
+    print(f"You like {food}")
+    food = input("Enter another food you like (q to quit): ")
+
+print("bye")
+
+
+num = int(input("Enter a number between 1 - 10: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a number between 1 - 10: "))
+
+print(f"Your number is {num}")
