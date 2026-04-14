f452e1bcb14d42a61134567947ef6d9d382f79ab
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-04-14 14:10:09 +0300

    Implement number guessing game with input validation, range checking and a guess counter

---
+++ lesson_04/number_guessing_game.py
@@ -0,0 +1,33 @@
+import random
+
+lowest_num = 1
+highest_num = 100
+
+answer = random.randint(lowest_num, highest_num)
+
+guesses = 0
+is_running = True
+
+print("Python Number Guessing Game")
+print(f"Select a number between {lowest_num} and {highest_num}")
+
+while is_running:
+    guess = input("Enter your guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+            print("This number is out of the range")
+            print(f"Select a number between {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too low! Try again")
+        elif guess > answer:
+            print("Too high! Try again")
+        else:
+            print(f"Correct! The answer was {answer}")
+            print(f"You guessed {guesses} times")
+    else:
+        print("Invalid guess")
+        print(f"Please select a number between {lowest_num} and {highest_num}")
