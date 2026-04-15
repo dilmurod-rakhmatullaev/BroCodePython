a1b9763cfd298c095489660219f1edfeaa13b661
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-04-15 15:56:46 +0300

    Practice modular programming and various import techniques

---
+++ lesson_05/functions_for_modules.py
@@ -0,0 +1,14 @@
+#used in modules.py
+pi = 3.14159
+
+def square(num):
+    return num ** 2
+
+def cube(num):
+    return num ** 3
+
+def circumference(radius):
+    return 2 * pi * radius
+
+def area(radius):
+    return pi * radius ** 2
---
+++ lesson_05/modules.py
@@ -0,0 +1,23 @@
+#import math
+import math as m
+
+
+# print(math.pi)
+print(m.sqrt(5))
+from math import e
+print(e)
+
+a, b, c, d, e = 1, 2, 3, 4, 5
+# now e = 5
+print(e ** a)
+print(e ** b)
+print(e ** c)
+print(e ** d)
+
+
+import functions_for_modules
+print(functions_for_modules.pi)
+print(functions_for_modules.square(3))
+print(functions_for_modules.cube(4))
+print(functions_for_modules.circumference(5))
+print(functions_for_modules.area(6))
