924ac274fabcb761d215eac9b3e5017ab1830f87
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-04-16 22:45:14 +0300

    Add Gangman game with ASCII art

---
+++ lesson_06/wordlist_for_hangman.py
@@ -0,0 +1,18 @@
+words = (
+    # Mammals
+    "lion", "tiger", "elephant", "giraffe", "zebra", "hippopotamus", "kangaroo",
+    "panda", "wolf", "fox", "bear", "monkey", "gorilla", "chimpanzee", "leopard",
+    "cheetah", "rhino", "camel", "deer", "rabbit", "squirrel", "raccoon", "otter",
+    "badger", "hedgehog", "sloth", "koala", "platypus", "bat", "whale", "dolphin",
+    "seal", "walrus", "hamster", "guinea pig", "horse", "cow", "sheep", "goat", "pig",
+
+    # Birds
+    "eagle", "falcon", "owl", "parrot", "penguin", "ostrich", "flamingo",
+    "peacock", "swan", "duck", "chicken", "turkey", "sparrow", "raven", "hummingbird",
+
+    # Reptiles and Amphibians
+    "crocodile", "alligator", "turtle", "snake", "lizard", "chameleon", "frog", "toad",
+
+    # Insects and Others
+    "butterfly", "bee", "ant", "spider", "scorpion", "crab", "lobster", "octopus"
+)
