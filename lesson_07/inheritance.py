ebe4f6f2d019d69145e11be971872d59fab28fcf
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-04-17 22:28:40 +0300

    Implement Animal hierarchy with inheritance

---
+++ lesson_07/inheritance.py
@@ -0,0 +1,34 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+class Mouse(Animal):
+    def speak(self):
+        print("Squeek!")
+
+dog = Dog("Rex")
+cat = Cat("Garfield")
+mouse = Mouse("Mickey")
+
+print(dog.name)
+print(cat.is_alive)
+mouse.eat()
+dog.sleep()
+dog.speak()
+cat.speak()
+mouse.speak()
