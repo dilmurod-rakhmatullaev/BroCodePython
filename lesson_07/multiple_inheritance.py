563c2a64c90f191c1d1c35a7dc1fb123b734933e
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-04-18 19:59:53 +0300

    Implement multiple inheritance with Animals

---
+++ lesson_07/multiple_inheritance.py
@@ -0,0 +1,42 @@
+class Animal:
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        print(f"This {self.name} is eating")
+
+    def sleep(self):
+        print(f"This {self.name} is sleeping")
+
+class Prey(Animal):
+    def flee(self):
+        print(f"This {self.name} is fleeing")
+
+class Predator(Animal):
+    def hunt(self):
+        print(f"This {self.name} is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit("Bugs")
+hawk = Hawk("Tony")
+fish = Fish("Nemo")
+print()
+
+rabbit.flee()
+hawk.hunt()
+fish.flee()
+fish.hunt()
+print()
+
+rabbit.sleep()
+hawk.eat()
+fish.eat()
