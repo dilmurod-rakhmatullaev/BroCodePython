934073e58d715fee1524c231d41f10b71193e338
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-04-22 14:10:12 +0300

    Add QCheckBox widget with state change handling

---
+++ lesson_10/pyqt5_checkboxes.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My first cool GUI")
+        self.setGeometry(700, 300, 500, 500)
+        self.checkbox = QCheckBox("Do you like pizza?", self)
+        self.initUI()
+
+    def initUI(self):
+        self.checkbox.setGeometry(10, 0, 500, 100)
+        self.checkbox.setStyleSheet("font-size: 30px"
+                                    "font-family: Times New Roman")
+        self.checkbox.setChecked(False)
+        self.checkbox.stateChanged.connect(self.checkbox_changed)
+
+    def checkbox_changed(self, state):
+        if state == Qt.Checked:
+            print("You like pizza")
+        else:
+            print("You do not like pizza")
+#
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
